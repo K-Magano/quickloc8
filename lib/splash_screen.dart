4e6f7f96d8e6784a91d340338b28d4f8ded38f72
Keorapetse Magano <maganokeo@gmail.com>
2024-02-03 12:26:05 +0200

    QuickLoc8 SplashScreen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+//import "package:flutter_config/flutter_config.dart";
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Quickloc8 Splash Screen',
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+//time
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(milliseconds: 3500),
+      () {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const Home()));
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xFFF55722),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/Quickloc8-logo.png",
+                height: 130,
+                width: 300,
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF55722),
+        title: const Text('Map page'),
+      ),
+    );
+  }
+}
